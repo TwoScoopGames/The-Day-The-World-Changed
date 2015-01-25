7543e8fe3dea3a9f460428912b0d5c63773bf93d
Eric Lathrop <eric@ericlathrop.com>
2015-01-25 13:51:43 -0500

    Add title screen.

--- game.js
+++ game.js
@@ -5,11 +5,6 @@ var context = canvas.getContext("2d");
 
 var Splat = require("splatjs");
 
-var main = new Splat.Scene();
-
-var entities = require("./entities");
-main.entities.load(entities);
-
 var animations = require("./animations");
 var input = new Splat.Input(require("./inputs"));
 
@@ -20,16 +15,10 @@ var sounds = new Splat.SoundLoader();
 console.log(sounds);
 sounds.loadFromManifest(require("./sounds"));
 
-var data = {
-	animations: animations,
-	canvas: canvas,
-	entities: main.entities,
-	images: images,
-	input: input,
-	sounds: sounds
-};
+var scenes = {};
+var systems = require("./systems");
 
-function installSystems(systems, ecs) {
+function installSystems(systems, ecs, data) {
 	systems.forEach(function(system) {
 		if (system.indexOf("splatjs:") === 0) {
 			var names = system.substr(8).split(".");
@@ -43,12 +32,54 @@ function installSystems(systems, ecs) {
 		}
 	});
 }
-var systems = require("./systems");
-installSystems(systems.simulation, main.simulation);
-installSystems(systems.renderer, main.renderer);
+
+var main = makeScene("main");
+main.entities.load(require("./entities"));
+
+function makeScene(name) {
+	var scene = new Splat.Scene();
+	var data = {
+		animations: animations,
+		canvas: canvas,
+		entities: scene.entities,
+		images: images,
+		input: input,
+		scenes: scenes,
+		sounds: sounds
+	};
+	scenes[name] = scene;
+	installSystems(systems.simulation, scene.simulation, data);
+	installSystems(systems.renderer, scene.renderer, data);
+	return scene;
+}
+
+var title = makeScene("title");
+title.renderer.add(function(entities, context) { // jshint ignore:line
+	if (input.button("left") || input.button("right")) {
+		title.stop();
+		main.start(context);
+	}
+});
+
+var img = title.entities.add();
+img.position = {
+	x: 0,
+	y: 0
+};
+img.image = {
+	name: "titlescreen",
+	sourceX: 0,
+	sourceY: 0,
+	sourceWidth: 1136,
+	sourceHeight: 640,
+	destinationX: 0,
+	destinationY: 0,
+	destinationWidth: 1136,
+	destinationHeight: 640
+};
 
 function percentLoaded() {
 	return (images.loadedImages + sounds.loadedSounds) / (images.totalImages + sounds.totalSounds);
 }
-var loading = Splat.loadingScene(canvas, percentLoaded, main);
+var loading = Splat.loadingScene(canvas, percentLoaded, title);
 loading.start(context);
